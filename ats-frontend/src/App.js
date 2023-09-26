59297bc7b687a2e07be1599041983546e58f56f0
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-09-26 12:42:01 +0200

    add website theme

--- ats-frontend/src/App.js
+++ ats-frontend/src/App.js
@@ -2,6 +2,26 @@ import LoginForm from "./components/LoginForm";
 import InfoTab from "./components/InfoTab";
 import TipsTab from "./components/TipsTab";
 
+import {
+  Container,
+  AppBar,
+  Button,
+  Toolbar,
+  createTheme,
+  ThemeProvider,
+} from "@mui/material";
+
+const websiteTheme = createTheme({
+  palette: {
+    primary: {
+      main: "#084c61",
+    },
+    secondary: {
+      main: "#FCA311",
+    },
+  },
+});
+
 function App() {
   return (
     <div>
