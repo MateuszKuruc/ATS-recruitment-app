25b10b0e715e22af86c8be46603a7cdc87a6a7e5
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-09-26 15:42:44 +0200

    add mock data to faq section to make it easier for tests

--- ats-frontend/src/components/InfoTab.js
+++ ats-frontend/src/components/InfoTab.js
@@ -1,5 +1,35 @@
 const InfoTab = () => {
-  return <div>FAQ</div>;
+  return (
+    <div>
+      <h1>FAQ</h1>
+      <p>
+        If you are facing any issues with the app or have some questions, you
+        might want to read the list of frequently asked questions below.
+      </p>
+      <h3>1. What is an ATS?</h3>
+      <p>
+        ATS stands for{" "}
+        <b>
+          <i>Applicant Tracking System</i>
+        </b>{" "}
+        and it is an automated tool for storing candidates' profiles with
+        details such as name, address, resume, skills, summary etc. Typically
+        such tools are used by recruitment agencies and most of the companies
+        that have their own recruitment branch.
+      </p>
+      <h3>2. How can I create an account?</h3>
+      <p>
+        In its current version, the application only offers access to the demo
+        mode with the ready-made account. Credentials and login instructions can
+        be found in the README file on GitHub.
+      </p>
+      <h3>3. Can I use this ATS app for free?</h3>
+      <p>
+        Yes, this is a completely free application that offers help in handling
+        day-to-day activities typical of recruiter's work.
+      </p>
+    </div>
+  );
 };
 
 export default InfoTab;
