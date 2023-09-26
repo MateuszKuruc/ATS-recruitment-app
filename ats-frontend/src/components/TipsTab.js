7725c9f762f21f5ea9cbea247b98a3c974985eb3
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-09-26 15:48:30 +0200

    add mock data for tips tab for improved development experience

--- ats-frontend/src/components/TipsTab.js
+++ ats-frontend/src/components/TipsTab.js
@@ -1,5 +1,22 @@
 const TipsTab = () => {
-  return <div>Tips</div>;
+  return (
+    <div>
+      <h1>Tips</h1>
+      <p>
+        This short guide was written with the aim of helping you successfully
+        navigate the application and make the best use of it. ATS apps are
+        becoming increasingly widespread and currently most, if not all,
+        recruitment agencies and internal recruitment teams use it, but the
+        concept of ATS is still foreign to most people outside of career in
+        recruitment.
+      </p>
+      <p>
+        Let us make clear how such systems are built and what their
+        functionalities are, so you can improve your recruiting experience and
+        keep track of the candidates' pipeline that you are working on.
+      </p>
+    </div>
+  );
 };
 
 export default TipsTab;
