f570ff08ad7f720d79a6f52966a4b6372424f04d
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-09-30 00:51:22 +0200

    style footer, add links

--- ats-frontend/src/components/Footer.js
+++ ats-frontend/src/components/Footer.js
@@ -6,36 +6,44 @@ import linkedinIcon from "../icons/linkedin.svg";
 const StyledPaper = styled(Paper)`
   padding: 2rem;
   margin-top: auto;
-  padding: 2rem;
-  text-align: center;
+  //   padding: 2rem;
+  //   text-align: center;
 `;
 
 const StyledImg = styled.img`
   width: 1rem;
 `;
 
+const StyledContainer = styled.div`
+  display: flex;
+  gap: 1rem;
+  justify-content: center;
+`;
+
 const Footer = () => {
   return (
     <StyledPaper style={{ background: "#084c61" }}>
-      <Container>
+      <StyledContainer>
         <Typography variant="h5">
           Applicant Tracking System by Mateusz Kuruc
-          <a
-            href="https://github.com/MateuszKuruc"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <StyledImg src={githubIcon} alt="GitHub Icon" />
-          </a>
-          <a
-            href="https://www.linkedin.com/in/mateuszkuruc/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <StyledImg src={linkedinIcon} alt="LinkedIn Icon" />
-          </a>
         </Typography>
-      </Container>
+
+        <a
+          href="https://github.com/MateuszKuruc"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <StyledImg src={githubIcon} alt="GitHub Icon" />
+        </a>
+
+        <a
+          href="https://www.linkedin.com/in/mateuszkuruc/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <StyledImg src={linkedinIcon} alt="LinkedIn Icon" />
+        </a>
+      </StyledContainer>
     </StyledPaper>
   );
 };
