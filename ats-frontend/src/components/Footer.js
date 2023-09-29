66de7516fd012d5f1f48bd7cc259fdf96507f967
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-09-29 20:30:33 +0200

    add github and linkedin svg icons and links to my profiles to the footer component

--- ats-frontend/src/components/Footer.js
+++ ats-frontend/src/components/Footer.js
@@ -1,7 +1,7 @@
 import { Container, Typography, Paper } from "@mui/material";
 import styled from "styled-components";
-import github from "../icons/github.svg";
-import linkedin from "../icons/linkedin.svg";
+import githubIcon from "../icons/github.svg";
+import linkedinIcon from "../icons/linkedin.svg";
 
 const StyledPaper = styled(Paper)`
   padding: 2rem;
@@ -10,12 +10,30 @@ const StyledPaper = styled(Paper)`
   text-align: center;
 `;
 
+const StyledImg = styled.img`
+  width: 1rem;
+`;
+
 const Footer = () => {
   return (
     <StyledPaper style={{ background: "#084c61" }}>
       <Container>
         <Typography variant="h5">
           Applicant Tracking System by Mateusz Kuruc
+          <a
+            href="https://github.com/MateuszKuruc"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <StyledImg src={githubIcon} alt="GitHub Icon" />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/mateuszkuruc/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <StyledImg src={linkedinIcon} alt="LinkedIn Icon" />
+          </a>
         </Typography>
       </Container>
     </StyledPaper>
