5c29239d4f3dc5ab2a89d912635fe48ec26c4107
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-10-02 13:19:14 +0200

    edit pools component to make the content fit the page better size wise

--- ats-frontend/src/components/Pools.js
+++ ats-frontend/src/components/Pools.js
@@ -3,6 +3,8 @@ import styled from "styled-components";
 
 const StyledPaper = styled(Paper)`
   height: 100%;
+  // height: 15rem;
+  // border: 5px solid red;
 `;
 
 const StyledButton = styled(Button)`
@@ -23,7 +25,7 @@ const StyledButton = styled(Button)`
 const Pools = () => {
   return (
     <div style={{ marginTop: "1rem", marginBottom: "1rem" }}>
-      <Grid container spacing={2} style={{ display: "flex", height: "100vh" }}>
+      <Grid container spacing={2} style={{ display: "flex", height: "75vh" }}>
         <Grid item xs={12} md={4}>
           <StyledPaper>
             <StyledButton variant="text">
