0aa9e1f3ec09d5cfa68b6babb5027a364c7aa819
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-10-04 20:33:54 +0200

    set up app js file, mongoose configuration, add express json parsing and cors handling

---
+++ ats-backend/app.js
@@ -0,0 +1,20 @@
+const config = require("./utils/config");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+const logger = require("./utils/logger");
+
+require("express-async-errors");
+
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info("connected to MongoDB");
+  })
+  .catch((error) => {
+    logger.error("error connecting to MongoDB:", error.message);
+  });
+
+app.use(cors());
+app.use(express.json());
