f97d78d1ae9dd700dd808e6c25c5cc8643c8349b
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-10-07 13:07:37 +0200

    implement user reducer in the redux store

--- ats-frontend/src/store.js
+++ ats-frontend/src/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import loginReducer from "./reducers/loginReducer";
 import candidateReducer from "./reducers/candidateReducer";
+import userReducer from "./reducers/userReducer";
 
 const store = configureStore({
   reducer: {
     login: loginReducer,
     candidates: candidateReducer,
+    user: userReducer,
   },
 });
 
