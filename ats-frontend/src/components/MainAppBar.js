cfd963ab942c6efa269b10794cd6853c370e696a
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-10-07 22:12:17 +0200

    style main appbar to include user profile icon and username. Move logout button to dropdown menu opened by clicking profile icon. Add feedback and help options, too

--- ats-frontend/src/components/MainAppBar.js
+++ ats-frontend/src/components/MainAppBar.js
@@ -1,12 +1,36 @@
 import { StyledAppBar, StyledToolBar, StyledButton } from "../App";
 import { Link } from "react-router-dom";
-import { Typography, Tooltip } from "@mui/material";
+import {
+  Typography,
+  Tooltip,
+  Popover,
+  List,
+  ListItem,
+  ListItemText,
+} from "@mui/material";
 import { useSelector } from "react-redux";
 import AccountBoxIcon from "@mui/icons-material/AccountBox";
+import { useState } from "react";
+import ExitToAppIcon from "@mui/icons-material/ExitToApp";
+import IconButton from "@mui/material/IconButton";
+import FeedbackIcon from "@mui/icons-material/Feedback";
+import ChatBubbleIcon from "@mui/icons-material/ChatBubble";
+import HelpIcon from '@mui/icons-material/Help';
 
 const MainAppBar = () => {
+  const [anchorEl, setAnchorEl] = useState(null);
   const login = useSelector((state) => state.login);
   console.log("login", login);
+
+  const open = Boolean(anchorEl);
+
+  const handleOpenMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleCloseMenu = (event) => {
+    setAnchorEl(null);
+  };
 
   return (
     <StyledAppBar
@@ -39,21 +63,74 @@ const MainAppBar = () => {
             <Typography variant="h4">Login</Typography>
           </StyledButton>
         ) : (
-          <div style={{ display: "flex", gap: "1rem" }}>
-            <div style={{ display: "flex", alignSelf: "center" }}>
-              <Tooltip title={`${login.username} logged in`}>
-                <AccountBoxIcon style={{ fontSize: "2rem" }} />
-              </Tooltip>
-            </div>
+          <div>
+            {/* <div > */}
+            {/* style={{ display: "flex", alignSelf: "center" }} */}
+            <Tooltip title={`${login.username} logged in`}>
+              <IconButton>
+                <AccountBoxIcon
+                  style={{ fontSize: "2rem" }}
+                  onClick={handleOpenMenu}
+                />
+              </IconButton>
+            </Tooltip>
+            <Popover
+              open={open}
+              anchorEl={anchorEl}
+              onClose={handleCloseMenu}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "right",
+              }}
+              transformOrigin={{
+                vertical: "top",
+                // horizontal: "right",
+              }}
+            >
+              <List>
+                <ListItem
+                  button
+                  component={Link}
+                  to="/feedback"
+                  onClick={handleCloseMenu}
+                >
+                  <ChatBubbleIcon />
+                  <ListItemText primary="Feedback" />
+                </ListItem>
+                <ListItem
+                  button
+                  component={Link}
+                  to="/help"
+                  onClick={handleCloseMenu}
+                >
+                  <HelpIcon />
+                  <ListItemText primary="Help" />
+                </ListItem>
+                <ListItem
+                  button
+                  component={Link}
+                  to="/logout"
+                  onClick={handleCloseMenu}
+                  style={{ paddingRight: "1rem" }} // Adjust padding
+                >
+                  <ExitToAppIcon />
+                  <ListItemText primary="Logout" />
+                </ListItem>
+              </List>
+            </Popover>
+            {/* </div> */}
 
-            <StyledButton
+            {/* <StyledButton
               color="secondary"
               variant="contained"
               component={Link}
               to="/logout"
             >
               <Typography variant="h4">Logout</Typography>
-            </StyledButton>
+            </StyledButton> */}
+            {/* <StyledButton variant="contained"> */}
+            <Typography variant="italic">{login.username}</Typography>
+            {/* </StyledButton> */}
           </div>
         )}
       </StyledToolBar>
