947b5fd934ed509aede43c878a1924cb57b69979
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-10-12 17:57:48 +0200

    change settimeout of notification message to 5 seconds instead of 3 for clarity

--- ats-frontend/src/components/Notification.js
+++ ats-frontend/src/components/Notification.js
@@ -18,7 +18,7 @@ const Notification = ({ severity, message }) => {
     if (notification.message) {
       const notificationTimer = setTimeout(() => {
         dispatch(removeNotification());
-      }, 3000);
+      }, 5000);
 
       return () => clearTimeout(notificationTimer);
     }
