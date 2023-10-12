5b7661e346972b0b6b24144a7b1b540165c32e4d
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-10-12 19:51:25 +0200

    add icon buttons to footer icons with links

--- ats-frontend/src/components/Footer.js
+++ ats-frontend/src/components/Footer.js
@@ -1,4 +1,4 @@
-import { Typography, Paper } from "@mui/material";
+import { Typography, Paper, IconButton } from "@mui/material";
 import styled from "styled-components";
 
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
@@ -23,22 +23,24 @@ const Footer = () => {
         <Typography variant="h5" style={{ color: "#FFFFFF" }}>
           Applicant Tracking System by Mateusz Kuruc
         </Typography>
-
-        <a
-          href="https://github.com/MateuszKuruc"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <GitHubIcon fontSize="large" color="action" />
-        </a>
-
-        <a
-          href="https://www.linkedin.com/in/mateuszkuruc/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <LinkedInIcon fontSize="large" color="action" />
-        </a>
+        <IconButton>
+          <a
+            href="https://github.com/MateuszKuruc"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <GitHubIcon fontSize="large" color="action" />
+          </a>
+        </IconButton>
+        <IconButton>
+          <a
+            href="https://www.linkedin.com/in/mateuszkuruc/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <LinkedInIcon fontSize="large" color="action" />
+          </a>
+        </IconButton>
       </StyledContainer>
     </StyledPaper>
   );
