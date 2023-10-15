18fad38cb56da799b42bce648854654d00bb36b0
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-10-16 00:45:14 +0200

    add responsive design to footer with conditional caption rendering to make it look good on mobile

--- ats-frontend/src/components/Footer.js
+++ ats-frontend/src/components/Footer.js
@@ -5,23 +5,42 @@ import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import GitHubIcon from "@mui/icons-material/GitHub";
 
 const StyledPaper = styled(Paper)`
-  padding: 2rem;
-  margin-top: auto;
+  && {
+    padding: 2rem;
+    margin-top: auto;
+
+    @media (max-width: 768px) {
+      padding: 0rem;
+      padding-top: 1rem;
+      padding-bottom: 1rem;
+    }
+  }
 `;
 
 const StyledContainer = styled.div`
-  display: flex;
-  gap: 1rem;
-  justify-content: center;
-  align-items: center;
+  && {
+    display: flex;
+    gap: 1rem;
+    justify-content: center;
+    align-items: center;
+
+    @media (max-width: 768px) {
+      gap: 0rem;
+    }
+  }
 `;
 
 const Footer = () => {
+  const caption =
+    window.innerWidth <= 768
+      ? "ATS by Mateusz Kuruc"
+      : "Applicant Tracking System by Mateusz Kuruc";
+
   return (
     <StyledPaper style={{ background: "#990033" }}>
       <StyledContainer>
         <Typography variant="h5" style={{ color: "#FFFFFF" }}>
-          Applicant Tracking System by Mateusz Kuruc
+          {caption}
         </Typography>
         <IconButton>
           <a
