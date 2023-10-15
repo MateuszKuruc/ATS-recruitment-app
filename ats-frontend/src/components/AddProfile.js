5eb2ca3daff34fe4d1fa00328732ec1bcbe09b56
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-10-16 00:13:38 +0200

    adjust add profile form to mobile display

--- ats-frontend/src/components/AddProfile.js
+++ ats-frontend/src/components/AddProfile.js
@@ -19,20 +19,20 @@ import { useNavigate } from "react-router-dom";
 import { validateEditForCandidate } from "../utils/validationService";
 import { setNotification } from "../reducers/notificationReducer";
 
-const FormContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  background-color: #ffffff;
-  border-radius: 0.5rem;
-  align-items: center;
-  padding: 2rem;
-  gap: 1rem;
-  // width: 90%;
+// const FormContainer = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   background-color: #ffffff;
+//   border-radius: 0.5rem;
+//   align-items: center;
+//   padding: 2rem;
+//   gap: 1rem;
+//   // width: 90%;
 
-  @media (min-width: 768px) {
-    // width: 40rem;
-  }
-`;
+//   @media (min-width: 768px) {
+//     // width: 40rem;
+//   }
+// `;
 
 const FieldContainer = styled.div`
   display: flex;
@@ -41,11 +41,11 @@ const FieldContainer = styled.div`
   padding: 3rem;
   border-radius: 0.5rem;
   gap: 1rem;
-  align-items: flex-start;
-  width: 90%;
+
+
 
   @media (min-width: 768px) {
-    width: 30rem;
+
   }
 `;
 
@@ -53,26 +53,24 @@ export const StyledTextField = styled(TextField)`
   && {
     background-color: #ffffff;
     border-radius: 0.5rem;
-    width: 100%;
   }
-`;
-
-const ComponentContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin-top: 1rem;
-  flex: 1;
-  margin-bottom: 1rem;
 `;
 
 const StyledForm = styled.form`
   display: flex;
   flex-direction: column;
-  align-items: center;
-  padding: 2rem;
-  padding-right: 1rem;
+  // align-items: center;
+  padding: 5rem;
+  padding-left: 12rem;
+  padding-right: 12rem;
   background-color: #c0d9e7;
+  margin-bottom: 1rem;
   flex: 1;
+  border-radius: 0.5rem;
+
+  @media (max-width: 768px) {
+    padding: 0;
+  }
 `;
 
 const StyledDatePicker = styled(DatePicker)`
@@ -82,7 +80,8 @@ const StyledDatePicker = styled(DatePicker)`
 const StyledButton = styled(Button)`
   && {
     padding: 1rem;
-    width: 100%;
+    border-radius: 0.5rem;
+
   }
 `;
 
@@ -185,110 +184,120 @@ const AddProfile = () => {
 
   return (
     // <ComponentContainer>
-      <StyledForm>
-        {/* <FormContainer> */}
-          <Typography variant="h4">NEW CANDIDATE FORM</Typography>
-          <FieldContainer>
-            <StyledTextField
-              error={firstNameError}
-              helperText={
-                firstNameError ? "Enter first name (min. 2 characters)" : ""
-              }
-              value={firstName}
-              label="First name"
-              onChange={({ target }) => setFirstName(target.value)}
-            ></StyledTextField>
+    <StyledForm>
+      {/* <FormContainer> */}
 
-            <StyledTextField
-              error={lastNameError}
-              helperText={
-                lastNameError ? "Enter last name (min. 2 characters)" : ""
-              }
-              value={lastName}
-              label="Last name"
-              onChange={({ target }) => setLastName(target.value)}
-            ></StyledTextField>
+      <FieldContainer>
+        <Typography variant="h4">NEW PROFILE FORM</Typography>
+        <StyledTextField
+          error={firstNameError}
+          helperText={
+            firstNameError ? "Enter first name (min. 2 characters)" : ""
+          }
+          value={firstName}
+          label="First name"
+          onChange={({ target }) => setFirstName(target.value)}
+        ></StyledTextField>
 
-            <StyledTextField
-              error={locationError}
-              helperText={locationError ? "Enter location" : ""}
-              value={location}
-              label="Location"
-              onChange={({ target }) => setLocation(target.value)}
-            ></StyledTextField>
+        <StyledTextField
+          error={lastNameError}
+          helperText={
+            lastNameError ? "Enter last name (min. 2 characters)" : ""
+          }
+          value={lastName}
+          label="Last name"
+          onChange={({ target }) => setLastName(target.value)}
+        ></StyledTextField>
 
-            <StyledTextField
-              error={emailError}
-              helperText={emailError ? "Enter email in correct format" : ""}
-              value={email}
-              label="Email address"
-              onChange={({ target }) => setEmail(target.value)}
-            ></StyledTextField>
+        <StyledTextField
+          error={locationError}
+          helperText={locationError ? "Enter location" : ""}
+          value={location}
+          label="Location"
+          onChange={({ target }) => setLocation(target.value)}
+        ></StyledTextField>
 
-            <StyledTextField
-              error={phoneError}
-              helperText={
-                phoneError ? "Enter phone number (9 - 11 characters)" : ""
-              }
-              value={phone}
-              label="Phone number"
-              onChange={({ target }) => setPhone(target.value)}
-            ></StyledTextField>
+        <StyledTextField
+          error={emailError}
+          helperText={emailError ? "Enter email in correct format" : ""}
+          value={email}
+          label="Email address"
+          onChange={({ target }) => setEmail(target.value)}
+        ></StyledTextField>
 
-            <FormControl fullWidth error={skillError}>
-              <InputLabel id="skill">Skill</InputLabel>
-              <Select
-                labelId="skill"
-                label="Skill"
-                value={skill}
-                onChange={({ target }) => setSkill(target.value)}
-              >
-                <MenuItem value="Java">Java</MenuItem>
-                <MenuItem value="Python">Python</MenuItem>
-                <MenuItem value="JavaScript">JavaScript</MenuItem>
-                <MenuItem value="C">C/C#/C++</MenuItem>
-                <MenuItem value="Scala">Scala</MenuItem>
-                <MenuItem value="BigData">Big Data</MenuItem>
-                <MenuItem value="DevOps">DevOps</MenuItem>
-                <MenuItem value="Mobile">Mobile</MenuItem>
-                <MenuItem value="Golang">Golang</MenuItem>
-              </Select>
-            </FormControl>
+        <StyledTextField
+          error={phoneError}
+          helperText={
+            phoneError ? "Enter phone number (9 - 11 characters)" : ""
+          }
+          value={phone}
+          label="Phone number"
+          onChange={({ target }) => setPhone(target.value)}
+        ></StyledTextField>
 
-            <FormControl fullWidth error={seniorityError}>
-              <InputLabel id="seniority">Seniority</InputLabel>
-              <Select
-                labelId="seniority"
-                label="Seniority"
-                value={seniority}
-                onChange={({ target }) => setSeniority(target.value)}
-              >
-                <MenuItem value="Intern">Intern</MenuItem>
-                <MenuItem value="Junior">Junior</MenuItem>
-                <MenuItem value="Regular">Regular</MenuItem>
-                <MenuItem value="Senior">Senior</MenuItem>
-                <MenuItem value="Lead">Lead</MenuItem>
-                <MenuItem value="Manager">Manager</MenuItem>
-              </Select>
-            </FormControl>
-
-            <StyledDatePicker
-              defaultValue={firstContact}
-              label="First contact"
-              onChange={(newValue) => setFirstContact(dayjs(newValue))}
-            />
-          </FieldContainer>
-
-          <StyledButton
-            type="submit"
-            id="addButton"
-            variant="contained"
-            onClick={handleNewCandidate}
+        <FormControl fullWidth error={skillError}>
+          <InputLabel id="skill">Skill</InputLabel>
+          <Select
+            labelId="skill"
+            label="Skill"
+            value={skill}
+            onChange={({ target }) => setSkill(target.value)}
           >
-            <Typography variant="h5">Add profile</Typography>
-          </StyledButton>
-        {/* </FormContainer> */}
-      </StyledForm>
+            <MenuItem value="Java">Java</MenuItem>
+            <MenuItem value="Python">Python</MenuItem>
+            <MenuItem value="JavaScript">JavaScript</MenuItem>
+            <MenuItem value="C">C/C#/C++</MenuItem>
+            <MenuItem value="Scala">Scala</MenuItem>
+            <MenuItem value="BigData">Big Data</MenuItem>
+            <MenuItem value="DevOps">DevOps</MenuItem>
+            <MenuItem value="Mobile">Mobile</MenuItem>
+            <MenuItem value="Golang">Golang</MenuItem>
+          </Select>
+        </FormControl>
+
+        <FormControl fullWidth error={seniorityError}>
+          <InputLabel id="seniority">Seniority</InputLabel>
+          <Select
+            labelId="seniority"
+            label="Seniority"
+            value={seniority}
+            onChange={({ target }) => setSeniority(target.value)}
+          >
+            <MenuItem value="Intern">Intern</MenuItem>
+            <MenuItem value="Junior">Junior</MenuItem>
+            <MenuItem value="Regular">Regular</MenuItem>
+            <MenuItem value="Senior">Senior</MenuItem>
+            <MenuItem value="Lead">Lead</MenuItem>
+            <MenuItem value="Manager">Manager</MenuItem>
+          </Select>
+        </FormControl>
+
+        <StyledDatePicker
+          defaultValue={firstContact}
+          label="First contact"
+          onChange={(newValue) => setFirstContact(dayjs(newValue))}
+        />
+
+        <StyledButton
+          type="submit"
+          id="addButton"
+          variant="contained"
+          onClick={handleNewCandidate}
+        >
+          <Typography variant="h5">Add profile</Typography>
+        </StyledButton>
+      </FieldContainer>
+
+      {/* <StyledButton
+        type="submit"
+        id="addButton"
+        variant="contained"
+        onClick={handleNewCandidate}
+      >
+        <Typography variant="h5">Add profile</Typography>
+      </StyledButton> */}
+      {/* </FormContainer> */}
+    </StyledForm>
     // </ComponentContainer>
   );
 };
