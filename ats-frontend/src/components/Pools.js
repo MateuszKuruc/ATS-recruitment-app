754cf89e45af39b7390484a0573cd30eecadab3c
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-10-16 09:31:41 +0200

    re-style pools component, remove grid and use flex instead to make responsive tiles adjustable to screen

--- ats-frontend/src/components/Pools.js
+++ ats-frontend/src/components/Pools.js
@@ -4,17 +4,29 @@ import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
 
 const StyledPaper = styled(Paper)`
-  height: 100%;
+  // height: 100%;
+  // width: 100%;
+  // aspect-ratio: 1 / 1;
+  // max-width: 300px;
+  display: flex;
+  flex: 1;
+
+  @media (max-width: 768px) {
+    aspect-ratio: auto;
+  }
 `;
 
 const StyledButton = styled(Button)`
   && {
-    width: 100%;
+    // width: 100%;
     background-color: #c0d9e7;
-    height: 100%;
+    // height: 100%;
     border-radius: 0.5rem;
     border: 0.3rem solid #084c61;
     transition: background-color 0.3s ease;
+
+    flex: 1;
+
     &:hover {
       background-color: #084c61;
       color: #ffffff;
@@ -23,15 +35,57 @@ const StyledButton = styled(Button)`
 `;
 
 const StyledGridContainer = styled(Grid)`
-  &.grid-container-class {
-    display: flex;
-    height: 75vh;
+  && {
+    align-self: center;
+    border: 1px solid red;
   }
 `;
 
 const StyledComponentContainer = styled.div`
-  margin-top: 1rem;
   margin-bottom: 1rem;
+  display: flex;
+  border: 1px solid blue;
+  justify-content: center;
+  align-items: center;
+  align-self: center;
+  padding-top: 1rem;
+  padding-bottom: 1rem;
+  padding-right: 3rem;
+  padding-left: 3rem;
+  // max-width: 1200px;
+
+  @media (max-width: 768px) {
+    padding: 0rem;
+
+    width: 100%;
+  }
+`;
+
+const MainContainer = styled.div`
+  && {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    height: 100%;
+    gap: 0.5rem;
+    margin-bottom: 1rem;
+
+    @margin (max-width: 768px) {
+
+    }
+  }
+`;
+
+const SecondaryContainer = styled.div`
+  && {
+    display: flex;
+    flex: 1;
+    gap: 0.5rem;
+
+    @media (max-width: 900px) {
+      flex-direction: column;
+    }
+  }
 `;
 
 const Pools = ({ setTechnology }) => {
@@ -43,105 +97,189 @@ const Pools = ({ setTechnology }) => {
   };
 
   return (
-    <StyledComponentContainer>
-      <StyledGridContainer
-        container
-        spacing={2}
-        className="grid-container-class"
-      >
-        <Grid item xs={12} md={4}>
-          <StyledPaper>
-            <StyledButton
-              variant="text"
-              onClick={() => handleTechnologyChoice("Java")}
-            >
-              <Typography variant="h1">Java</Typography>
-            </StyledButton>
-          </StyledPaper>
-        </Grid>
-        <Grid item xs={12} md={4}>
-          <StyledPaper>
-            <StyledButton
-              variant="text"
-              onClick={() => handleTechnologyChoice("Python")}
-            >
-              <Typography variant="h1">Python</Typography>
-            </StyledButton>
-          </StyledPaper>
-        </Grid>
-        <Grid item xs={12} md={4}>
-          <StyledPaper>
-            <StyledButton
-              variant="text"
-              onClick={() => handleTechnologyChoice("JavaScript")}
-            >
-              <Typography variant="h1">JavaScript</Typography>
-            </StyledButton>
-          </StyledPaper>
-        </Grid>
-        <Grid item xs={12} md={4}>
-          <StyledPaper>
-            <StyledButton
-              variant="text"
-              onClick={() => handleTechnologyChoice("C")}
-            >
-              <Typography variant="h1">C/C#/C++</Typography>
-            </StyledButton>
-          </StyledPaper>
-        </Grid>
-        <Grid item xs={12} md={4}>
-          <StyledPaper>
-            <StyledButton
-              variant="text"
-              onClick={() => handleTechnologyChoice("Scala")}
-            >
-              <Typography variant="h1">Scala</Typography>
-            </StyledButton>
-          </StyledPaper>
-        </Grid>
-        <Grid item xs={12} md={4}>
-          <StyledPaper>
-            <StyledButton
-              variant="text"
-              onClick={() => handleTechnologyChoice("BigData")}
-            >
-              <Typography variant="h1">Big Data</Typography>
-            </StyledButton>
-          </StyledPaper>
-        </Grid>
-        <Grid item xs={12} md={4}>
-          <StyledPaper>
-            <StyledButton
-              variant="text"
-              onClick={() => handleTechnologyChoice("DevOps")}
-            >
-              <Typography variant="h1">DevOps</Typography>
-            </StyledButton>
-          </StyledPaper>
-        </Grid>
-        <Grid item xs={12} md={4}>
-          <StyledPaper>
-            <StyledButton
-              variant="text"
-              onClick={() => handleTechnologyChoice("Mobile")}
-            >
-              <Typography variant="h1">Mobile</Typography>
-            </StyledButton>
-          </StyledPaper>
-        </Grid>
-        <Grid item xs={12} md={4}>
-          <StyledPaper>
-            <StyledButton
-              variant="text"
-              onClick={() => handleTechnologyChoice("Golang")}
-            >
-              {" "}
-              <Typography variant="h1">Golang</Typography>
-            </StyledButton>
-          </StyledPaper>
-        </Grid>
-      </StyledGridContainer>
-    </StyledComponentContainer>
+    // <StyledComponentContainer>
+    //   <StyledGridContainer container spacing={1}>
+    //     <Grid item xs={12} sm={12} md={4}>
+    //       <StyledPaper>
+    //         <StyledButton
+    //           variant="text"
+    //           onClick={() => handleTechnologyChoice("Java")}
+    //         >
+    //           <Typography variant="h1">Java</Typography>
+    //         </StyledButton>
+    //       </StyledPaper>
+    //     </Grid>
+    //     <Grid item xs={12} sm={12} md={4}>
+    //       <StyledPaper>
+    //         <StyledButton
+    //           variant="text"
+    //           onClick={() => handleTechnologyChoice("Python")}
+    //         >
+    //           <Typography variant="h1">Python</Typography>
+    //         </StyledButton>
+    //       </StyledPaper>
+    //     </Grid>
+    //     <Grid item xs={12} sm={12} md={4}>
+    //       <StyledPaper>
+    //         <StyledButton
+    //           variant="text"
+    //           onClick={() => handleTechnologyChoice("JavaScript")}
+    //         >
+    //           <Typography variant="h1">JavaScript</Typography>
+    //         </StyledButton>
+    //       </StyledPaper>
+    //     </Grid>
+    //     <Grid item xs={12} sm={12} md={4}>
+    //       <StyledPaper>
+    //         <StyledButton
+    //           variant="text"
+    //           onClick={() => handleTechnologyChoice("C")}
+    //         >
+    //           <Typography variant="h1">C/C#/C++</Typography>
+    //         </StyledButton>
+    //       </StyledPaper>
+    //     </Grid>
+    //     <Grid item xs={12} sm={12} md={4}>
+    //       <StyledPaper>
+    //         <StyledButton
+    //           variant="text"
+    //           onClick={() => handleTechnologyChoice("Scala")}
+    //         >
+    //           <Typography variant="h1">Scala</Typography>
+    //         </StyledButton>
+    //       </StyledPaper>
+    //     </Grid>
+    //     <Grid item xs={12} sm={12} md={4}>
+    //       <StyledPaper>
+    //         <StyledButton
+    //           variant="text"
+    //           onClick={() => handleTechnologyChoice("BigData")}
+    //         >
+    //           <Typography variant="h1">Big Data</Typography>
+    //         </StyledButton>
+    //       </StyledPaper>
+    //     </Grid>
+    //     <Grid item xs={12} sm={12} md={4}>
+    //       <StyledPaper>
+    //         <StyledButton
+    //           variant="text"
+    //           onClick={() => handleTechnologyChoice("DevOps")}
+    //         >
+    //           <Typography variant="h1">DevOps</Typography>
+    //         </StyledButton>
+    //       </StyledPaper>
+    //     </Grid>
+    //     <Grid item xs={12} sm={12} md={4}>
+    //       <StyledPaper>
+    //         <StyledButton
+    //           variant="text"
+    //           onClick={() => handleTechnologyChoice("Mobile")}
+    //         >
+    //           <Typography variant="h1">Mobile</Typography>
+    //         </StyledButton>
+    //       </StyledPaper>
+    //     </Grid>
+    //     <Grid item xs={12} sm={12} md={4}>
+    //       <StyledPaper>
+    //         <StyledButton
+    //           variant="text"
+    //           onClick={() => handleTechnologyChoice("Golang")}
+    //         >
+    //           {" "}
+    //           <Typography variant="h1">Golang</Typography>
+    //         </StyledButton>
+    //       </StyledPaper>
+    //     </Grid>
+    //   </StyledGridContainer>
+    // </StyledComponentContainer>
+
+    <MainContainer>
+      <SecondaryContainer>
+        <StyledPaper>
+          <StyledButton
+            variant="text"
+            onClick={() => handleTechnologyChoice("Java")}
+          >
+            <Typography variant="h1">Java</Typography>
+          </StyledButton>
+        </StyledPaper>
+
+        <StyledPaper>
+          <StyledButton
+            variant="text"
+            onClick={() => handleTechnologyChoice("Python")}
+          >
+            <Typography variant="h1">Python</Typography>
+          </StyledButton>
+        </StyledPaper>
+
+        <StyledPaper>
+          <StyledButton
+            variant="text"
+            onClick={() => handleTechnologyChoice("JavaScript")}
+          >
+            <Typography variant="h1">JavaScript</Typography>
+          </StyledButton>
+        </StyledPaper>
+      </SecondaryContainer>
+      <SecondaryContainer>
+        <StyledPaper>
+          <StyledButton
+            variant="text"
+            onClick={() => handleTechnologyChoice("C")}
+          >
+            <Typography variant="h1">C/C#/C++</Typography>
+          </StyledButton>
+        </StyledPaper>
+
+        <StyledPaper>
+          <StyledButton
+            variant="text"
+            onClick={() => handleTechnologyChoice("Scala")}
+          >
+            <Typography variant="h1">Scala</Typography>
+          </StyledButton>
+        </StyledPaper>
+
+        <StyledPaper>
+          <StyledButton
+            variant="text"
+            onClick={() => handleTechnologyChoice("BigData")}
+          >
+            <Typography variant="h1">Big Data</Typography>
+          </StyledButton>
+        </StyledPaper>
+      </SecondaryContainer>
+      <SecondaryContainer>
+        <StyledPaper>
+          <StyledButton
+            variant="text"
+            onClick={() => handleTechnologyChoice("DevOps")}
+          >
+            <Typography variant="h1">DevOps</Typography>
+          </StyledButton>
+        </StyledPaper>
+
+        <StyledPaper>
+          <StyledButton
+            variant="text"
+            onClick={() => handleTechnologyChoice("Mobile")}
+          >
+            <Typography variant="h1">Mobile</Typography>
+          </StyledButton>
+        </StyledPaper>
+
+        <StyledPaper>
+          <StyledButton
+            variant="text"
+            onClick={() => handleTechnologyChoice("Golang")}
+          >
+            {" "}
+            <Typography variant="h1">Golang</Typography>
+          </StyledButton>
+        </StyledPaper>
+      </SecondaryContainer>
+    </MainContainer>
   );
 };
 
