7a18def404612f615b9334b9bfd4a5cbdf069ade
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-10-19 11:18:02 +0200

    finish styling home page component

--- ats-frontend/src/components/Info/HomePage.js
+++ ats-frontend/src/components/Info/HomePage.js
@@ -1,6 +1,6 @@
 import AnimatedPage from "../Layout/AnimatedPage";
 import styled from "styled-components";
-import { Typography } from "@mui/material";
+import { Typography, Paper } from "@mui/material";
 import { Link } from "react-router-dom";
 
 const MainContainer = styled.div`
@@ -9,9 +9,13 @@ const MainContainer = styled.div`
   margin-bottom: 1rem;
   padding: 2rem;
   border-radius: 0.5rem;
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
 
   @media (max-width: 768px) {
     padding: 0.5rem;
+    gap: 0rem;
   }
 `;
 const Header = styled.div`
@@ -25,19 +29,36 @@ const Header = styled.div`
 
   @media (max-width: 768px) {
     padding: 0.5rem;
+    margin-bottom: 0.5rem;
+    margin-top: 0.5rem;
   }
 `;
 
-const Content = styled.div``;
-
 const HeaderTypography = styled(Typography)`
   color: #ffba49;
-  color: white;
-  // color: #8F3985;
+  color: #ffffff;
 `;
 
 const HeaderCaption = styled(Typography)`
   color: #ffba49;
+`;
+
+const StyledPaper = styled(Paper)`
+  padding: 2rem;
+  margin-bottom: 1rem;
+
+  @media (max-width: 768px) {
+    padding: 0.5rem;
+  }
+`;
+
+const BottomContainer = styled.div`
+  margin-top: 1rem;
+  background-color: #ffba49;
+  padding: 1rem;
+  display: flex;
+  justify-content: center;
+  border-radius: 0.5rem;
 `;
 
 const HomePage = () => {
@@ -55,19 +76,20 @@ const HomePage = () => {
             Get your pipeline under control!
           </HeaderCaption>
         </Header>
-        <Content>
-          <Typography variant="body1">
-            <p>
-              ATS is a valuable tool in the recruiter's arsenal, allowing
-              intuitive data storage, feedback management and status control.
-            </p>
-            <p>
-              The application was deisgned <em>specifically</em> with IT sector
-              recruitment in mind. It is reflected in the field types of the
-              candidate profile, technologies in pools section and job titles.
-            </p>
-          </Typography>
+        <Typography variant="body1">
+          <p>
+            ATS is a valuable tool in the recruiter's arsenal, allowing
+            intuitive data storage, feedback management and status control.
+          </p>
+          <p>
+            The application was deisgned <em>specifically</em> with IT sector
+            recruitment in mind. It is reflected in the field types of the
+            candidate profile, technologies in pools section and job titles.
+          </p>
+        </Typography>
+        <StyledPaper>
           <Typography variant="h6">Core features:</Typography>
+
           <Typography variant="body1">
             <ul>
               <li>Add new candidate profile with basic details</li>
@@ -75,26 +97,24 @@ const HomePage = () => {
               <li>Upload and delete candidate CV</li>
               <li>Update or remove candidate profiles</li>
               <li>Display and sort the list of all candidates</li>
-              <li>Show the list of 'hot' profiles based on assessment</li>
+              <li>Show the list of 'hot' profiles based on assessment score</li>
               <li>Show pools with division based on technologies</li>
               <li>Display basic statistics about each pool</li>
             </ul>
           </Typography>
-          <Typography variant="italic">
-            For more details visit the <Link>Tips</Link> and <Link>FAQ </Link>
-            tabs
-          </Typography>
-        </Content>
+        </StyledPaper>
+        <Typography variant="italic">
+          For more details visit the <Link to="/tips">Tips</Link> and{" "}
+          <Link to="/faq">FAQ </Link>
+          tabs. To leave feedback, visit{" "}
+          <Link to="userfeedback">Feedback </Link>tab.
+        </Typography>
+        <BottomContainer>
+          <Typography variant="h4">ENJOY!</Typography>
+        </BottomContainer>
       </MainContainer>
     </AnimatedPage>
   );
 };
 
 export default HomePage;
-
-<div>
-  <h1>Applicant Tracking System - get your pipeline under control!</h1>
-  <p>
-    <i>Log in to see your pipeline and work on new placements</i>
-  </p>
-</div>;
