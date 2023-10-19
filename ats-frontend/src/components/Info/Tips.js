f403a09939307c96734e535d804195555cef658f
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-10-19 12:07:04 +0200

    finish styling tips component for desktop and mobile screens

--- ats-frontend/src/components/Info/Tips.js
+++ ats-frontend/src/components/Info/Tips.js
@@ -10,6 +10,10 @@ const MainContainer = styled.div`
   flex: 1;
   margin-bottom: 1rem;
   border-radius: 0.5rem;
+
+  @media (max-width: 768px) {
+    padding: 0.5rem;
+  }
 `;
 
 const Intro = styled.div``;
@@ -20,6 +24,10 @@ const Content = styled.div`
 `;
 const StyledPaper = styled(Paper)`
   padding: 1rem;
+
+  @media (max-width: 768px) {
+    padding: 0.5rem;
+  }
 `;
 
 const Header = styled.div`
@@ -30,6 +38,12 @@ const Header = styled.div`
   background-color: #25283d;
   padding: 2rem;
   border-radius: 0.5rem;
+
+  @media (max-width: 768px) {
+    padding: 0.5rem;
+    margin-bottom: 0.5rem;
+    margin-top: 0.5rem;
+  }
 `;
 
 const HeaderTypography = styled(Typography)`
@@ -143,43 +157,3 @@ const TipsTab = () => {
 };
 
 export default TipsTab;
-
-<div>
-  <h1>Tips</h1>
-  <p>
-    This short guide was written with the aim of helping you successfully
-    navigate the application and make the best use of it. ATS apps are becoming
-    increasingly widespread and currently most, if not all, recruitment agencies
-    and internal recruitment teams use it, but the concept of ATS is still
-    foreign to most people outside of career in recruitment.
-  </p>
-  <p>
-    Let us make clear how such systems are built and what their functionalities
-    are, so you can improve your recruiting experience and keep track of the
-    candidates' pipeline that you are working on.
-  </p>
-  <h2>What are advantages of using ATS?</h2>
-  <p>
-    First of all, ATS is a way of keeping all the important details regarding
-    candidates in one place. It eliminates the need to employ additional
-    separate trackers (e.g. in Excel), which makes it a very universal tool.
-  </p>
-
-  <div>
-    <p>Some of the funcionalities include:</p>
-    <ul>
-      <li>Adding, removing and editing candidate profiles</li>
-      <li>Uploading resumes and other related documents to a profile</li>
-      <li>
-        Tracking recruitment process stages to quickly gather information on the
-        given candidate's progress in terms of hiring
-      </li>
-      <li>
-        Acting as a database of candidates for the future reference. If the
-        candidate has been rejected from one process, they could still be a
-        perfect match for the future recruitments - ATS allows easier access to
-        such individuals
-      </li>
-    </ul>
-  </div>
-</div>;
