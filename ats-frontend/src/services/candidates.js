b02ab48939d354b6c44eee1d8fa2c8e0560e35e4
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-10-19 18:57:59 +0200

    new build

--- ats-frontend/src/services/candidates.js
+++ ats-frontend/src/services/candidates.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 
-// const baseUrl = "/api/candidates";  before deployment, only local
+const baseUrl = "/api/candidates";  
+// before deployment, only local
 
 // const baseUrl = "https://ats-backend.onrender.com/api/candidates"; fails
 
-const baseUrl = "https://ats-backend-dvrg.onrender.com/api/candidates";
+// const baseUrl = "https://ats-backend-dvrg.onrender.com/api/candidates"; works
 
 const generateUniqueFilename = (originalFilename) => {
   const timestamp = new Date().getTime();
--- ats-frontend/src/services/login.js
+++ ats-frontend/src/services/login.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 
-// const baseUrl = "/api/login"; local
+const baseUrl = "/api/login"; 
+// local
 
 // const baseUrl = "https://ats-backend.onrender.com/api/login";  fails
 
-const baseUrl = "https://ats-backend-dvrg.onrender.com/api/login";
+// const baseUrl = "https://ats-backend-dvrg.onrender.com/api/login";
 
 const login = async (credentials) => {
   const response = await axios.post(baseUrl, credentials);
