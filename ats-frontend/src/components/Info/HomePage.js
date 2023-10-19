233863cf05c3be792dec904be10405af63febfe4
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-10-19 10:33:41 +0200

    set up basic outline of home page

--- ats-frontend/src/components/Info/HomePage.js
+++ ats-frontend/src/components/Info/HomePage.js
@@ -1,28 +1,100 @@
 import AnimatedPage from "../Layout/AnimatedPage";
 import styled from "styled-components";
+import { Typography } from "@mui/material";
+import { Link } from "react-router-dom";
 
 const MainContainer = styled.div`
+  background-color: #ebcbf4;
+  flex: 1;
+  margin-bottom: 1rem;
+  padding: 2rem;
+  border-radius: 0.5rem;
 
-`
+  @media (max-width: 768px) {
+    padding: 0.5rem;
+  }
+`;
+const Header = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: #25283d;
+  padding: 2rem;
+  border-radius: 0.5rem;
+
+  @media (max-width: 768px) {
+    padding: 0.5rem;
+  }
+`;
+
+const Content = styled.div``;
+
+const HeaderTypography = styled(Typography)`
+  color: #ffba49;
+  color: white;
+  // color: #8F3985;
+`;
+
+const HeaderCaption = styled(Typography)`
+  color: #ffba49;
+`;
 
 const HomePage = () => {
-  return (
-   <AnimatedPage>
-    <MainContainer>
+  const title =
+    window.innerWidth <= 768
+      ? "ATS FOR IT RECRUITERS"
+      : "APPLICANT TRACKING SYSTEM FOR IT RECRUITERS";
 
-    </MainContainer>
+  return (
+    <AnimatedPage>
+      <MainContainer>
+        <Header>
+          <HeaderTypography variant="h4">{title}</HeaderTypography>
+          <HeaderCaption variant="italic3" style={{ color: "#FFBA49" }}>
+            Get your pipeline under control!
+          </HeaderCaption>
+        </Header>
+        <Content>
+          <Typography variant="body1">
+            <p>
+              ATS is a valuable tool in the recruiter's arsenal, allowing
+              intuitive data storage, feedback management and status control.
+            </p>
+            <p>
+              The application was deisgned <em>specifically</em> with IT sector
+              recruitment in mind. It is reflected in the field types of the
+              candidate profile, technologies in pools section and job titles.
+            </p>
+          </Typography>
+          <Typography variant="h6">Core features:</Typography>
+          <Typography variant="body1">
+            <ul>
+              <li>Add new candidate profile with basic details</li>
+              <li>Provide written feedback after the meeting with candidate</li>
+              <li>Upload and delete candidate CV</li>
+              <li>Update or remove candidate profiles</li>
+              <li>Display and sort the list of all candidates</li>
+              <li>Show the list of 'hot' profiles based on assessment</li>
+              <li>Show pools with division based on technologies</li>
+              <li>Display basic statistics about each pool</li>
+            </ul>
+          </Typography>
+          <Typography variant="italic">
+            For more details visit the <Link>Tips</Link> and <Link>FAQ </Link>
+            tabs
+          </Typography>
+        </Content>
+      </MainContainer>
     </AnimatedPage>
   );
 };
 
 export default HomePage;
 
-
-
-
 <div>
-      <h1>Applicant Tracking System - get your pipeline under control!</h1>
-      <p>
-        <i>Log in to see your pipeline and work on new placements</i>
-      </p>
-    </div>
+  <h1>Applicant Tracking System - get your pipeline under control!</h1>
+  <p>
+    <i>Log in to see your pipeline and work on new placements</i>
+  </p>
+</div>;
