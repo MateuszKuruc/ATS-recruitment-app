6639a26c3b82310f48de37e5fbbb268d597e9653
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-10-19 14:09:20 +0200

    install express

--- ats-backend/index.js
+++ ats-backend/index.js
@@ -1,7 +1,7 @@
 const config = require("./utils/config");
 const app = require("./app");
 
-const PORT = config.PORT;
+const PORT = process.env.PORT || config.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
