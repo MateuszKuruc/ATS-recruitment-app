3bdb33bf160cf90cc9de710f3daa15f3b50fc1b7
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-10-19 18:32:08 +0200

    test of setting the rendering of react frontend from backend folder

--- ats-backend/app.js
+++ ats-backend/app.js
@@ -10,8 +10,6 @@ const loginRouter = require("./controllers/login");
 const middleware = require("./utils/middleware");
 require("express-async-errors");
 
-// const frontendDomain = "https://ats-mateuszkuruc.onrender.com/";
-
 mongoose
   .connect(config.MONGODB_URI)
   .then(() => {
@@ -26,17 +24,19 @@ app.use(middleware.errorHandler);
 
 app.use(cors());
 
-// app.use(
-//   cors({
-//     origin: frontendDomain,
-//   })
-// );
-
 app.use(express.json());
 
 app.use("/api/candidates", candidatesRouter);
 app.use("/api/users", usersRouter);
 app.use("/api/login", loginRouter);
 app.use("/uploads", express.static("uploads"));
+
+// serve static react site
+
+app.use(express.static("build"));
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 module.exports = app;
