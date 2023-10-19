d4d50460d8909291f8969d275efd7900259fbbb4
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-10-19 18:52:39 +0200

    static serving of react app change

--- ats-backend/app.js
+++ ats-backend/app.js
@@ -35,7 +35,7 @@ app.use("/uploads", express.static("uploads"));
 
 app.use(express.static("build"));
 
-app.get("/*", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
