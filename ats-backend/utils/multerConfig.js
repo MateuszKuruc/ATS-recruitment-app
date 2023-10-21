8360e76765c25c1192290431be9961a740fdef9f
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-10-21 10:31:44 +0200

    update multer config to handle aws requests

--- ats-backend/utils/multerConfig.js
+++ ats-backend/utils/multerConfig.js
@@ -1,21 +1,3 @@
-// const multer = require("multer");
-
-// const storage = multer.diskStorage({
-//   destination: function (req, file, cb) {
-//     cb(null, "uploads/"); // Set the destination directory for uploaded files
-//   },
-//   filename: function (req, file, cb) {
-//     // cb(null, Date.now() + '-' + file.originalname); // Set the filename
-//     cb(null, file.originalname);
-//   },
-// });
-
-// const upload = multer({ storage: storage });
-
-// module.exports = upload;
-
-// AWS
-
 const multer = require("multer");
 
 const upload = multer();
